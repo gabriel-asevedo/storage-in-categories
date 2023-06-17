3405ef02d7e74fdc9ccfaca111668e6975299a70
Gabriel Asevedo <gabrielamorim002@gmail.com>
2023-06-17 17:29:07 -0300

    new project

---
+++ src/test/java/com/storagedepartments/StoragedepartmentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.storagedepartments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoragedepartmentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
